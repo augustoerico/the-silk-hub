2cdc3ed62aa072b62b9f1878ed9ce42a34f21dc1
Erico Silva <augusto.ericosilva@gmail.com>
2017-08-27 18:22:45 -0300

    Fix dependencies conflict

--- app/src/main/java/com/cgbros/silkhub/LoginActivity.kt
+++ app/src/main/java/com/cgbros/silkhub/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.cgbros.silkhub
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_login.*
@@ -40,6 +41,56 @@ class LoginActivity : AppCompatActivity() {
             login_open_sessions.text = text
         }
 
+    }
+
+    inner class FacebookLoginCallback : FacebookCallback<LoginResult> {
+
+        override fun onSuccess(result: LoginResult?) {
+            Log.d("login", "Facebook login success")
+            handleAccessToken(result?.accessToken)
+        }
+
+        override fun onCancel() {
+            Log.d("login", "Facebook login canceled")
+        }
+
+        override fun onError(error: FacebookException?) {
+            Log.e("login", "Error on FacebookLoginCallback", error)
+            // TODO send user some feedback
+        }
+
+        private fun handleAccessToken(accessToken: AccessToken?): Unit {
+
+            val token: String = accessToken?.token ?: ""
+            Log.d("login", "Handle Facebook access accessToken [$token]")
+
+            if (token.isNotEmpty()) {
+                val credentials = FacebookAuthProvider.getCredential(token)
+                mAuth.signInWithCredential(credentials)
+                        .addOnCompleteListener { authResult ->
+                            Log.d("login", "On complete listener")
+                            if (authResult.isSuccessful) {
+                                Log.d("login", "... is successful")
+                                val result = authResult.result
+                                val user = result.user
+
+                                Log.d("login", "user: $user")
+
+                                val intent: Intent = Intent(that, MainActivity::class.java)
+                                startActivity(intent)
+                            } else {
+                                Log.e("login", "Error on complete listener",
+                                        authResult.exception)
+                                // TODO send user some feedback
+                            }
+                        }
+            } else {
+                val message = "Empty access token"
+                Log.e("login", message, Exception(message))
+                // TODO send user some feedback
+            }
+
+        }
     }
 
 }
